85bbe78457f5b2eca111f2eec0c43af8e4461d4b
karpov-anatolii <schodya@gmail.com>
2023-10-09 17:45:30 +0300

    docker-compose

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,11 @@
-const ApiError = require("../error/ApiError");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const { User, Basket } = require("../models/models");
+const ApiError = require('../error/ApiError');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { User, Basket } = require('../models/models');
 
 const generateJwt = (id, email, role) => {
   return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
-    expiresIn: "24h",
+    expiresIn: '24h',
   });
 };
 
@@ -13,21 +13,21 @@ class UserController {
   async registration(req, res, next) {
     const { email, password, role } = req.body;
     if (!email || !password) {
-      return next(ApiError.badRequest("Некоректний email чи password!"));
+      return next(ApiError.badRequest('Некоректний email чи password!'));
     }
     const candidate = await User.findOne({ where: { email } });
     if (candidate) {
-      return next(ApiError.badRequest("Користувач із таким email вже існує"));
+      return next(ApiError.badRequest('Користувач із таким email вже існує'));
     }
 
     const hashPassword = await bcrypt.hash(password, 5);
 
-    const admin = await User.findOne({ where: { role: "ADMIN" } });
+    const admin = await User.findOne({ where: { role: 'ADMIN' } });
     let user;
     if (!admin) {
       user = await User.create({
         email,
-        role: "ADMIN",
+        role: 'ADMIN',
         password: hashPassword,
       });
     } else {
@@ -41,11 +41,11 @@ class UserController {
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email } });
     if (!user) {
-      return next(ApiError.internal("Користувач не знайдений"));
+      return next(ApiError.internal('Користувач не знайдений'));
     }
     let comparePassword = bcrypt.compareSync(password, user.password); // сравниваем захешированный пароль введенный юзером с уже захешированным паролем из базы данных
     if (!comparePassword) {
-      return next(ApiError.internal("Користувач не знайдений"));
+      return next(ApiError.internal('Користувач не знайдений'));
     }
     const token = generateJwt(user.id, user.email, user.role);
     return res.json({ token });
@@ -56,7 +56,7 @@ class UserController {
     const token = generateJwt(req.user.id, req.user.email, req.user.role);
     return res.json({
       token,
-      res: "Генерация нового токена",
+      res: 'Генерация нового токена',
       email: req.user.email,
     });
   }
